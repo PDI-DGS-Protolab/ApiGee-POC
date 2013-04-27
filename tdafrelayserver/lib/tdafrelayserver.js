9e737d19bf86e13ae8672d4d4f4b21512267a934
Javier Mendiara Cañardo <javier@tid.es>
2013-04-27 18:30:03 +0200

    First Commit

---
+++ tdafrelayserver/lib/tdafrelayserver.js
@@ -0,0 +1,11 @@
+'use strict';
+
+
+/**
+ * The exported functions and properties for tdafrelayserver
+ * @return {String}
+ *   The condition of the module.
+ */
+exports.awesome = function() {
+  return 'awesome';
+};
---
+++ tdafrelayserver/tools/init-site-env.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git clone https://github.com/PDI-DGS-Protolab/ApiGee-POC.git site
+cd site
+git checkout gh-pages
